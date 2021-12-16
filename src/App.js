dd73874626bf3007d4a065ccb7fa6f74a043c327
shakesbeer2049 <max.zubi@gmail.com>
2021-12-16 22:07:55 +0530

    c0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState, useEffect } from "react";
+import Note from "./Components/Note";
+import axios from "axios";
+import noteService from './Services/note'
+import './index.css'
 
-function App() {
+const App = (props) => {
+
+  const [notes, setNotes] = useState([]);
+  const [newNote, setNewNote] = useState("");
+  const [showAll, setShowAll] = useState(false);
+
+  //Toggle Important
+  const toggleImpt = (id) =>{
+    console.log(`Importance of ${id} needs to be toggled`);
+    const url = `http://localhost:3001/notes/${id}`
+    const note = notes.find(n => n.id === id)
+    const changedNote = {...note, important: !note.important}
+
+   noteService
+   .update(id,changedNote)
+   .then(returnedNote => {
+     setNotes(notes.map(note=> note.id!==id?note:returnedNote))
+   })
+  }
+
+ 
+  const ip = document.querySelector("input");
+
+  //ADD NOTE
+  function addNote(e) {
+    e.preventDefault();
+    if (ip.value === "") {
+      window.alert("Input can't be empty");
+    } else {
+      const noteObj = {
+        content: newNote,
+        date: new Date().toISOString(),
+        important: Math.random() < 0.5,
+      };
+     noteService
+        .create(noteObj)
+        .then(createdNote => {
+          setNotes(notes.concat(createdNote));
+          console.log(createdNote)
+        })
+
+      console.log(notes);
+      setNewNote("");
+    }
+  }
+
+  //Get All Notes
+  useEffect(()=>{
+    noteService.getAll()
+    .then(notes=> {
+      setNotes(notes)
+    })
+   }
+    ,[notes.length])
+  
+ 
+
+  function ipHandler(e) {
+    setNewNote(e.target.value);
+  }
+  function viewer() {
+    setShowAll(!showAll);
+  }
+
+//Display Notes
+const notesToShow = showAll ? notes : notes.filter((note) => note.important);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+       <form className="form" onSubmit={addNote}>
+        <input className="form-input" type="text" value={newNote} onChange={ipHandler} />
+        <button className="form-btn" type="submit">Save</button>
+      </form>
+       <button className="toggle-btn" onClick={viewer}>{showAll?'Show Important':'Show All'}</button>
+      
+      
+      <ul className="note-container">
+
+        {notesToShow.map((note,i) => (
+         <Note 
+         key={i}
+         note = {note}
+         toggle = {()=>toggleImpt(note.id)} />
+        ))}
+      </ul>
+
+     
     </div>
   );
-}
+};
 
 export default App;
